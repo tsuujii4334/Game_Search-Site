590cb1990086adf609ee417ac3ab44ff571775ef
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-10-08 09:26:21 +0000

    [update]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-# 顧客用
+# ユーザー用
 # URL /user/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -11,7 +11,7 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
 
-  namespace :public do
+  scope module: :public do
     root to: 'homes#top'
     get 'user/:id/bookmarks' => 'bookmarks#index', as: 'index_bookmark'
     get 'reviews/:id' => 'reviews#show', as: 'show_review'
@@ -24,26 +24,26 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
     get 'users/mypage' => 'users#show', as: 'mypage_user'
     get 'users/edit' => 'users#edit', as: 'edit_user'
     patch 'users' => 'users#update', as: 'update_user'
-    get 'users/confirm'
-    patch 'users#withdrawal'
+    get 'users/confirm' => 'users#confirm'
+    patch 'users/withdrawal' => 'users#withdrawal'
     get '/' => 'homes#top'
   end
 
   namespace :admin do
     get 'genres' => 'genres#index'
-    post 'genres/create' => 'genres'
+    post 'genres/create' => 'genres#create'
     get 'genres/:id/edit' => 'genres#edit', as: 'edit_genre'
-    patch 'genres/:id' => 'genres#update', ad: 'update_genre'
+    patch 'genres/:id' => 'genres#update', as: 'update_genre'
     get 'users' => 'users#index'
     get 'users/:id' => 'users#show', as: 'show_user'
     get 'users/:id/edit' => 'users#edit', as: 'edit_user'
     patch 'users/:id' => 'users#update', as: 'update_user'
     get 'games' => 'games#index'
-    get 'games#new'
+    get 'games/new' => 'games#new'
     post 'games' => 'games#create', as: 'create_game'
     get 'games/:id' => 'games#show', as: 'show_game'
     get 'games/:id/edit' => 'games#edit', as: 'edit_game'
-    patch 'games/:id' => 'games#update', aa: 'update_game'
+    patch 'games/:id' => 'games#update', as: 'update_game'
     get 'reviews/:id' => 'reviews#show', as: 'show_review'
     delete 'reviews/:id' => 'reviews#destroy', as: 'destroy_review'
     delete 'comments/:id' => 'comments#destroy',as: 'destroy_comment'

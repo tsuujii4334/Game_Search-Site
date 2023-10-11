545a47d6a1e8cfa7b8c4291ab9699a1a303ddc9e
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-10-11 08:58:58 +0000

    [update]views/admin/games/index.html

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,5 +1,9 @@
 class Admin::GamesController < ApplicationController
   def index
+    @gamecount = Game.all.count
+    @games = Game.all.page(params[:page]).per(8)
+    @game = Game.where(price: 1..1000 )#値段で絞り込む機能を実装途中
+    # @game = Game("games.price DESC")
   end
 
   def new

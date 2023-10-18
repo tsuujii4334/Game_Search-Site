060bf565b33eca90fed5408badb12c93dcf4ab85
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-10-18 16:53:41 +0000

    [update]migrate

--- db/migrate/20231005111629_create_games.rb
+++ db/migrate/20231005111629_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
       t.integer :genre_id, null: false
-      t.integer :user_id, null: false
+      t.integer :user_id
       t.string :name, null: false
       t.text :introduction, null:false
       t.integer :price, null:false
